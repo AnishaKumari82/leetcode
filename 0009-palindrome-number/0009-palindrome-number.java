1e97c87d53537cad35693eb9794dd4f2c5c00464
Anisha Kumari <anishabrnwl@gmail.com>
2025-12-15 13:20:03 +0530

    Time: 5 ms (73.72%), Space: 46 MB (33.52%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -2,18 +2,20 @@ class Solution {
     public boolean isPalindrome(int x) {
         int revno = 0;
         int dup = x;
+        if (x < 0) {
+            return false;
+        }
 
-        while(x>0){
-            int ld = x%10;
+        while(x != 0){
+            int ld= x%10;
+            if (revno > Integer.MAX_VALUE / 10 || revno < Integer.MIN_VALUE / 10) {
+                return false; // Return 0 in case of overflow
+            }
             revno = revno*10+ld;
             x = x/10;
         
         }
-        if(revno==dup){
-            return true;
-        }else{
-            return false;
-        }
+        return( dup == revno);
         
     }
 }
