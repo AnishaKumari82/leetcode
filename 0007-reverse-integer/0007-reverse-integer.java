c9943a4ad4b228ec3e9cb55ba979a72214c0a8a2
Anisha Kumari <anishabrnwl@gmail.com>
2025-12-23 12:39:07 +0530

    Time: 1 ms (97.36%), Space: 42.5 MB (66.82%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -2,8 +2,10 @@ class Solution {
     public int reverse(int n) {
         int revno=0;
         while(n != 0){
-        
             int ld= n%10;
+            if (revno > Integer.MAX_VALUE / 10 || revno < Integer.MIN_VALUE / 10) {
+                return 0; // Return 0 in case of overflow
+            }
             revno= revno*10+ld;
             n=n/10;
         }
