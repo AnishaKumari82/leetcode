2e360d85847a676826b76a97c0ba57bc5d9d922e
Anisha Kumari <anishabrnwl@gmail.com>
2025-12-24 20:47:36 +0530

    Time: 0 ms (100%), Space: 42.9 MB (59.06%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,8 +1,55 @@
+// tc- O(n)
+// sc-O(n^2)
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         String[] words = s.split(" ");
+//         return words[words.length-1].length();
+        
+//     }
+// }
+
+
+
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] words = s.split(" ");
-        return words[words.length-1].length();
+        int n = s.length();
+        int cnt=0;
+        for(int i= s.length()-1 ; i>=0; i--){
+            if(s.charAt(i) != ' '){
+                cnt++;
 
-        
+            }else{
+                if(cnt>0){
+                    return cnt;
+                }
+            }
+        }
+
+        return cnt;
     }
 }
+
+
+
+
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         int i = s.length() - 1;
+//         int count = 0;
+
+//         // Step 1: Skip trailing spaces
+//         while (i >= 0 && s.charAt(i) == ' ') {
+//             i--;
+//         }
+
+//         // Step 2: Count characters of last word
+//         while (i >= 0 && s.charAt(i) != ' ') {
+//             count++;
+//             i--;
+//         }
+
+//         return count;
+//     }
+// }
+
+
