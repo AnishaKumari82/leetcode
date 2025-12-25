50aed67d2aee686548956a3c38592dec5432dae2
Anisha Kumari <anishabrnwl@gmail.com>
2025-12-25 11:20:12 +0530

    Time: 3 ms (99.73%), Space: 175.7 MB (12.13%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,28 +1,55 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
+        int[] res = new int[n];
 
-        int[] pos = new int[n / 2];
-        int[] neg = new int[n / 2];
+        int posIdx = 0;
+        int negIdx = 1;
 
-        int p = 0, q = 0;  
-        //we can use static array but we can to take p and q to add the elements otherwise use arraylist to use arr.add() function
-
-        // Step 1: Separate positives and negatives
         for (int i = 0; i < n; i++) {
             if (nums[i] > 0) {
-                pos[p++] = nums[i];
+                res[posIdx] = nums[i];
+                posIdx += 2;
             } else {
-                neg[q++] = nums[i];
+                res[negIdx] = nums[i];
+                negIdx += 2;
             }
         }
 
-        // Step 2: Rearrange alternately
-        for (int i = 0; i < n / 2; i++) {
-            nums[2 * i] = pos[i];
-            nums[2 * i + 1] = neg[i];
-        }
-
-        return nums;
+        return res;
     }
 }
+
+
+
+
+
+//BRUTE FORCE APPROACH
+// class Solution {
+//     public int[] rearrangeArray(int[] nums) {
+//         int n = nums.length;
+
+//         int[] pos = new int[n / 2];
+//         int[] neg = new int[n / 2];
+
+//         int p = 0, q = 0;  
+//         //we can use static array but we can to take p and q to add the elements otherwise use arraylist to use arr.add() function
+
+//         // Step 1: Separate positives and negatives
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] > 0) {
+//                 pos[p++] = nums[i];
+//             } else {
+//                 neg[q++] = nums[i];
+//             }
+//         }
+
+//         // Step 2: Rearrange alternately
+//         for (int i = 0; i < n / 2; i++) {
+//             nums[2 * i] = pos[i];
+//             nums[2 * i + 1] = neg[i];
+//         }
+
+//         return nums;
+//     }
+// }
