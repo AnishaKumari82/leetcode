792dda75d839437168816626a5b25c0b95ac94ec
Anisha Kumari <anishabrnwl@gmail.com>
2026-01-08 01:06:11 +0530

    Time: 0 ms (100%), Space: 44.2 MB (10.2%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,13 +1,20 @@
 class Solution {
-    public boolean searchinrow(int[] nums, int k){
+    public boolean searchMatrix(int[][] nums, int k){
         int n = nums.length;
+        int m = nums[0].length;
         int low =0;
-        int high = n-1;
+        int high = n* m-1;
+
         while(low<=high){
         int mid = (low+high)/2;
-        if(nums[mid]==k){
+
+        
+        int row = mid /m;
+        int col = mid%m;
+
+        if(nums[row][col]==k){
             return true;
-        }else if(nums[mid] > k){
+        }else if(nums[row][col] > k){
             high= mid-1;
         }else{
             low=mid+1;
@@ -15,15 +22,36 @@ class Solution {
     }
     return false;
     }
-        public boolean searchMatrix(int[][] matrix, int target) {
-            int n = matrix.length;
-            int m = matrix[0].length;
-
-            for(int i=0;i<n;i++){
-                if(target >=matrix[i][0] && target <= matrix[i][m-1]){
-                    return searchinrow(matrix[i] , target);
-                }
-            }
-            return false;
-    }
 }
+        
+
+
+// class Solution {
+//     public boolean searchinrow(int[] nums, int k){
+//         int n = nums.length;
+//         int low =0;
+//         int high = n-1;
+//         while(low<=high){
+//         int mid = (low+high)/2;
+//         if(nums[mid]==k){
+//             return true;
+//         }else if(nums[mid] > k){
+//             high= mid-1;
+//         }else{
+//             low=mid+1;
+//         }
+//     }
+//     return false;
+//     }
+//         public boolean searchMatrix(int[][] matrix, int target) {
+//             int n = matrix.length;
+//             int m = matrix[0].length;
+
+//             for(int i=0;i<n;i++){
+//                 if(target >=matrix[i][0] && target <= matrix[i][m-1]){
+//                     return searchinrow(matrix[i] , target);
+//                 }
+//             }
+//             return false;
+//     }
+// }
